5fa81f862cddcc3e86eaf32e74b277a464a765cd
hakomori64 <hakomori64@gmail.com>
2021-12-09 19:41:00 +0900

    localhost to 0.0.0.0

--- entity/src/bin/listener.rs
+++ entity/src/bin/listener.rs
@@ -5,6 +5,6 @@ fn main() {
     println!("starting client server...");
     set_current_dir_to_executable_directory().unwrap();
 
-    let socket_server = SocketServer::new("localhost", 8081);
+    let socket_server = SocketServer::new("0.0.0.0", S8081);
     socket_server.start(handle_connection);
 }
